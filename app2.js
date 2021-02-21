7495ead648bb633cd9ea5596222f9c7f552e9cb3
Yun <763506525@qq.com>
2021-02-21 11:24:18 +0800

    1

--- app2.js
+++ app2.js
@@ -1,6 +1,6 @@
 var spawn = require('child_process').spawn;
 
-free = spawn('hexo', ['server', '-d' ]);/* 其实就是等于执行hexo server -p 4000*/
+free = spawn('hexo', ['server', '-d', "-p", "8080"]);/* 其实就是等于执行hexo server -p 4000*/
 
 free.stdout.on('data', function (data) {
 
